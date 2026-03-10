20db2c5f268c5981bbc3c4729a280e6cebd99039
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 16:49:26 +0800

    add components on form

--- taks1/Form1.Designer.cs
+++ taks1/Form1.Designer.cs
@@ -28,12 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textVector1x = new TextBox();
+            textVector2x = new TextBox();
+            textVector1y = new TextBox();
+            textVector1z = new TextBox();
+            textVector2y = new TextBox();
+            textVector2z = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Вектор 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Вектор 2";
+            // 
+            // textVector1x
+            // 
+            textVector1x.Location = new Point(101, 22);
+            textVector1x.Name = "textVector1x";
+            textVector1x.Size = new Size(31, 23);
+            textVector1x.TabIndex = 2;
+            // 
+            // textVector2x
+            // 
+            textVector2x.Location = new Point(101, 49);
+            textVector2x.Name = "textVector2x";
+            textVector2x.Size = new Size(31, 23);
+            textVector2x.TabIndex = 3;
+            // 
+            // textVector1y
+            // 
+            textVector1y.Location = new Point(138, 22);
+            textVector1y.Name = "textVector1y";
+            textVector1y.Size = new Size(31, 23);
+            textVector1y.TabIndex = 4;
+            // 
+            // textVector1z
+            // 
+            textVector1z.Location = new Point(175, 22);
+            textVector1z.Name = "textVector1z";
+            textVector1z.Size = new Size(31, 23);
+            textVector1z.TabIndex = 5;
+            // 
+            // textVector2y
+            // 
+            textVector2y.Location = new Point(138, 49);
+            textVector2y.Name = "textVector2y";
+            textVector2y.Size = new Size(31, 23);
+            textVector2y.TabIndex = 6;
+            // 
+            // textVector2z
+            // 
+            textVector2z.Location = new Point(175, 49);
+            textVector2z.Name = "textVector2z";
+            textVector2z.Size = new Size(31, 23);
+            textVector2z.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(223, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Длинна вектора 1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(223, 52);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Длинна вектора 2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(456, 20);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(41, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(456, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(41, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(18, 126);
+            label5.Name = "label5";
+            label5.Size = new Size(117, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Результат операций";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 123);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(226, 23);
+            textBox3.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(18, 85);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 14;
+            button1.Text = "Сложение";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(99, 85);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 15;
+            button2.Text = "Вычитание";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(180, 85);
+            button3.Name = "button3";
+            button3.Size = new Size(155, 23);
+            button3.TabIndex = 16;
+            button3.Text = "Скалярное произведение";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(341, 85);
+            button4.Name = "button4";
+            button4.Size = new Size(156, 23);
+            button4.TabIndex = 17;
+            button4.Text = "Векторное произведение";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(333, 19);
+            button5.Name = "button5";
+            button5.Size = new Size(117, 23);
+            button5.TabIndex = 18;
+            button5.Text = "Длинна вектора 1";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(333, 48);
+            button6.Name = "button6";
+            button6.Size = new Size(117, 23);
+            button6.TabIndex = 19;
+            button6.Text = "Длинна вектора 2";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(101, 4);
+            label6.Name = "label6";
+            label6.Size = new Size(14, 15);
+            label6.TabIndex = 20;
+            label6.Text = "X";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(138, 4);
+            label7.Name = "label7";
+            label7.Size = new Size(14, 15);
+            label7.TabIndex = 21;
+            label7.Text = "Y";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(175, 4);
+            label8.Name = "label8";
+            label8.Size = new Size(14, 15);
+            label8.TabIndex = 22;
+            label8.Text = "Z";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(516, 169);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textVector2z);
+            Controls.Add(textVector2y);
+            Controls.Add(textVector1z);
+            Controls.Add(textVector1y);
+            Controls.Add(textVector2x);
+            Controls.Add(textVector1x);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textVector1x;
+        private TextBox textVector2x;
+        private TextBox textVector1y;
+        private TextBox textVector1z;
+        private TextBox textVector2y;
+        private TextBox textVector2z;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
