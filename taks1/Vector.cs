6783e773ba57961bb8c2e00f33443b959187a116
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 15:54:48 +0800

    add files and constryktor

---
+++ taks1/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taks1
+{
+    public class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public string Verbose()
+        {
+            return String.Format("{0} {1} {2}", this.x = x, this.y = y, this.z = z);
+        }
+    }
+}
---
+++ taks1Tests/VectorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using taks1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taks1.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var length = new Vector(10, 1,1);
+            Assert.AreEqual("10 1 1", length.Verbose());
+        }
+    }
+}
