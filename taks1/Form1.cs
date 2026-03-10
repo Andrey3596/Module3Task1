35a800793f10a8c7262891a065a3465692e10843
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 19:41:42 +0800

    fix DRY

--- taks1/Form1.cs
+++ taks1/Form1.cs
@@ -17,7 +17,7 @@ namespace taks1
             InitializeComponent();
         }
 
-        private void textVector1x_TextChanged(object sender, EventArgs e)
+        private void calculateVector1length()
         {
             try
             {
@@ -33,109 +33,57 @@ namespace taks1
             }
             catch (FormatException)
             {
-                
+
             }
+        }
+        private void calculateVector2length()
+        {
+            try
+            {
+                var vector2x = double.Parse(textVector2x.Text);
+
+                var vector2y = double.Parse(textVector2y.Text);
+
+                var vector2z = double.Parse(textVector2z.Text);
+
+                Vector vector2 = new Vector(vector2x, vector2y, vector2z);
+
+                textVectorlength2.Text = vector2.Vectorlength().ToString();
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void textVector1x_TextChanged(object sender, EventArgs e)
+        {
+            calculateVector1length();
         }
 
         private void textVector2x_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                var vector2x = double.Parse(textVector2x.Text);
-
-                var vector2y = double.Parse(textVector2y.Text);
-
-                var vector2z = double.Parse(textVector2z.Text);
-
-                Vector vector2 = new Vector(vector2x, vector2y, vector2z);
-
-                textVectorlength2.Text = vector2.Vectorlength().ToString();
-            }
-            catch (FormatException)
-            {
-
-            }
+            calculateVector2length();
         }
 
         private void textVector1y_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                var vector1x = double.Parse(textVector1x.Text);
-
-                var vector1y = double.Parse(textVector1y.Text);
-
-                var vector1z = double.Parse(textVector1z.Text);
-
-                Vector vector1 = new Vector(vector1x, vector1y, vector1z);
-
-                textVectorlength1.Text = vector1.Vectorlength().ToString();
-
-            }
-            catch (FormatException)
-            {
-
-            }
+            calculateVector1length();
         }
 
         private void textVector2y_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                var vector2x = double.Parse(textVector2x.Text);
-
-                var vector2y = double.Parse(textVector2y.Text);
-
-                var vector2z = double.Parse(textVector2z.Text);
-
-                Vector vector2 = new Vector(vector2x, vector2y, vector2z);
-
-                textVectorlength2.Text = vector2.Vectorlength().ToString();
-            }
-            catch (FormatException)
-            {
-
-            }
+            calculateVector2length();
         }
 
         private void textVector1z_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                var vector1x = double.Parse(textVector1x.Text);
-
-                var vector1y = double.Parse(textVector1y.Text);
-
-                var vector1z = double.Parse(textVector1z.Text);
-
-                Vector vector1 = new Vector(vector1x, vector1y, vector1z);
-
-                textVectorlength1.Text = vector1.Vectorlength().ToString();
-            }
-            catch (FormatException)
-            {
-
-            }
+            calculateVector1length();
         }
 
         private void textVector2z_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                var vector2x = double.Parse(textVector2x.Text);
-
-                var vector2y = double.Parse(textVector2y.Text);
-
-                var vector2z = double.Parse(textVector2z.Text);
-
-                Vector vector2 = new Vector(vector2x, vector2y, vector2z);
-
-                textVectorlength2.Text = vector2.Vectorlength().ToString();
-            }
-            catch (FormatException)
-            {
-
-            }
+            calculateVector2length();
         }
     }
 }
